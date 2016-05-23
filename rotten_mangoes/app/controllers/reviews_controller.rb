d8e571c779f2084c30a7e2bb7c4c84592b890183
Rayed <siddiqui.rayed@gmail.com>
2016-05-23 18:45:43 -0400

    refactoring, final commit

--- rotten_mangoes/app/controllers/reviews_controller.rb
+++ rotten_mangoes/app/controllers/reviews_controller.rb
@@ -1,13 +1,15 @@
 class ReviewsController < ApplicationController
-  
+
+  before_filter :restrict_access
+
+  before_filter :load_movie
+
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
-    @review = @movie.reviews.build(review_params) # this is the same as @review = Review.new(movie_id: @movie.id)
+    @review = @movie.reviews.build(review_params)
     @review.user_id = current_user.id
 
     if @review.save
@@ -18,6 +20,10 @@ class ReviewsController < ApplicationController
   end
 
   protected
+
+  def load_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 
   def review_params
     params.require(:review).permit(:text, :rating_out_of_ten)
