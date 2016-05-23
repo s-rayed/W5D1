3a17aa17ad827e6f4538b757d1ceb1a1a7c90cc2
Rayed S <siddiqui.rayed@gmail.com>
2016-05-23 17:01:23 +0000

    first commit

---
+++ lighthouse_forum/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '16b69fee1ae246199be08fe58f0dc77b23817be95c9d57eb6f6dc4ccd914d8de52d646f187a1be65f986baf1371cb99688a1c87b070a2b616b11d10ad9ab900b'
