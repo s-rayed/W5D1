93294b2ab0ccca72fc2acf6e5f66adab17ff5294
Rayed <siddiqui.rayed@gmail.com>
2016-05-23 15:53:17 -0400

    added delete functionality and completed exercise

--- lighthouse_forum/config/routes.rb
+++ lighthouse_forum/config/routes.rb
@@ -54,13 +54,8 @@ LighthouseForum::Application.routes.draw do
   #     resources :products
   #   end
 
-get 'posts', to: 'posts#index'
-
-get 'posts/new', to: 'posts#new'
-get 'posts/:id/edit', to: 'posts#edit'
-get 'posts/:id', to: 'posts#show', as:'post' # necessary for the update action.
-patch 'posts/:id', to: 'posts#update'
-post 'posts', to: 'posts#create'
+  resources :posts
+  root to: 'posts#index'
   # [tons of helpful comments]
 
 end
