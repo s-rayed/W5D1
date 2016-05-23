f6e9f267d0ed8bc9a16377c17528c2e0cde8c6cc
Rayed <siddiqui.rayed@gmail.com>
2016-05-23 15:17:21 -0400

    added new, create methods

--- lighthouse_forum/config/routes.rb
+++ lighthouse_forum/config/routes.rb
@@ -56,7 +56,9 @@ LighthouseForum::Application.routes.draw do
 
 get 'posts', to: 'posts#index'
 
+get 'posts/new', to: 'posts#new'
 get 'posts/:id', to: 'posts#show'
+post 'posts', to: 'posts#create'
   # [tons of helpful comments]
 
 end
