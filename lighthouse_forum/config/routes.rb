feff2c6e9417980e362c4e173aa5008a528016bf
Rayed <siddiqui.rayed@gmail.com>
2016-05-23 15:25:34 -0400

    added editing functionality to posts

--- lighthouse_forum/config/routes.rb
+++ lighthouse_forum/config/routes.rb
@@ -57,7 +57,9 @@ LighthouseForum::Application.routes.draw do
 get 'posts', to: 'posts#index'
 
 get 'posts/new', to: 'posts#new'
-get 'posts/:id', to: 'posts#show'
+get 'posts/:id/edit', to: 'posts#edit'
+get 'posts/:id', to: 'posts#show', as:'post' # necessary for the update action.
+patch 'posts/:id', to: 'posts#update'
 post 'posts', to: 'posts#create'
   # [tons of helpful comments]
 
