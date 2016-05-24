1da6eebba9cc423e84bb25d0bf36a4e0d3c99436
Rayed S <siddiqui.rayed@gmail.com>
2016-05-24 19:35:45 +0000

    completed...for the most part

--- rotten_mangoes/app/models/movie.rb
+++ rotten_mangoes/app/models/movie.rb
@@ -14,16 +14,15 @@ class Movie < ActiveRecord::Base
   validates :description,
     presence: true
 
-  validates :poster_image_url,
-    presence: true
-
   validates :release_date,
     presence: true
 
   validate :release_date_is_in_the_past
 
+  mount_uploader :image, ImageUploader
+
   def review_average
-    reviews.sum(:rating_out_of_ten)/reviews.size unless 
+    reviews.sum(:rating_out_of_ten)/reviews.size unless reviews.nil?
   end
 
   protected
