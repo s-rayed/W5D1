fad8f79e8240e2a14e3c908285b5eb472cf49fe1
Rayed <siddiqui.rayed@gmail.com>
2016-05-25 17:50:49 -0400

    added movie-search and delete user from admin features

--- rotten_mangoes/app/controllers/admin/users_controller.rb
+++ rotten_mangoes/app/controllers/admin/users_controller.rb
@@ -5,4 +5,10 @@ class Admin::UsersController < ApplicationController
     @users = User.page(params[:page]).per(5)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
 end
